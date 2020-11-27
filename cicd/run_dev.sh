6f751a5ca685b12a359e8953e22ee079e7efe04e
tutkarma <tutkarma@gmail.com>
2020-11-28 00:09:14 +0300

    Add build and run client for dev running

--- cicd/run_dev.sh
+++ cicd/run_dev.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.dev.yml build
-docker-compose -f docker-compose.dev.yml up -d --remove-orphans
+if [ -z "$1" ]; then
+    docker-compose -f docker-compose.dev.yml build
+    docker-compose -f docker-compose.dev.yml up -d --remove-orphans
+elif [ "$1" = "--server" ]; then
+    docker-compose -f docker-compose.dev.yml build core
+    docker-compose -f docker-compose.dev.yml up -d --remove-orphans core
+elif [ "$1" = "--client" ]; then
+    docker-compose -f docker-compose.dev.yml build client
+    docker-compose -f docker-compose.dev.yml up -d --remove-orphans client
+fi
