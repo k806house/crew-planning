e4c02bb55f2070c9d02fe1b42df3d2bffb918fe1
juliavav <julia.vav@gmail.com>
2020-11-28 17:56:20 +0300

    added: added options v1

--- client/cp-frontend/src/app/app.module.ts
+++ client/cp-frontend/src/app/app.module.ts
@@ -8,9 +8,17 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import { TopbarComponent } from './topbar/toolbar.component';
+import {TopbarComponent} from './topbar/toolbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {OptionsComponent} from './options/options.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {DatePipe} from '@angular/common';
 
 
 @NgModule({
@@ -29,9 +37,21 @@ import {OptionsComponent} from './options/options.component';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatDividerModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSelectModule
 
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/cp-frontend/src/app/options/options.component.ts
+++ client/cp-frontend/src/app/options/options.component.ts
@@ -1,4 +1,12 @@
 import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {Train, TrainPrintable} from '../_models';
+import {DatePipe} from '@angular/common';
+
+interface Route {
+  value: number;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-options',
@@ -6,11 +14,69 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./options.component.less']
 })
 export class OptionsComponent implements OnInit {
+  addForm: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder, public datepipe: DatePipe) {
+  }
+
+  currentInterval: string;
+  intervals: string[] = ['Следущая неделя', 'Следущий месяц', 'Другой'];
+  currentRout: string;
+  routs: Route[] = [
+    {value: 1, viewValue: 'Самара – Пенза 1'},
+    {value: 2, viewValue: 'Пенза 1 – Самара'}
+  ];
+
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+
+  trainsToDelete: Train[] = [];
+  trainsToAdd: TrainPrintable[] = [];
+
+  get f() {
+    return this.addForm.controls;
   }
 
   ngOnInit(): void {
+    this.addForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      time: ['', Validators.required],
+      route: ['', Validators.required],
+      date: ['', Validators.required],
+
+    });
   }
 
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    console.log(this.f);
+    const train = new TrainPrintable();
+    train.title = this.f.title.value;
+    train.date = this.datepipe.transform(this.f.date.value, 'dd/MM/yyyy');
+    train.route = this.f.route.value;
+    train.time = this.f.time.value;
+    this.trainsToAdd.push(train);
+    return 0;
+    // this.trainsToAdd
+    // this.submitted = true;
+    //
+    // // stop here if form is invalid
+    // if (this.loginForm.invalid) {
+    //   return;
+    // }
+    //
+    // this.loading = true;
+    // this.authenticationService.login(this.f.username.value, this.f.password.value)
+    //   .pipe(first())
+    //   .subscribe(
+    //     data => {
+    //       this.router.navigate([this.returnUrl]);
+    //     },
+    //     error => {
+    //       this.error = error;
+    //       this.loading = false;
+    //     });
+  }
 }
